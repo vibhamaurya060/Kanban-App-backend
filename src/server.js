614c3e989a61cdf4fbc26ea7e08f2bd3196f1145
Vibha Maurya <vibhamaurya060@gmail.com>
2024-07-07 13:36:27 +0530

    added cors to backend

--- src/server.js
+++ src/server.js
@@ -5,11 +5,14 @@ import ConnectToDb from './config/db.js';
 import userRouter from './routes/userRoute.js';
 import taskRouter from './routes/taskRoute.js';
 import auth from './middlewares/auth.js';
+import cors from 'cors';
+
 config();
 const app=express();
 const port = process.env.PORT;
 const db_uri=process.env.DB_URI 
 
+app.use(cors({origin:process.env.FRONTEND_URL, credentials:true}))
 app.use(express.json());
 
 app.get('/',(req,res)=>{
